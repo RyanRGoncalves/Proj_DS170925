b89e0cd556bd61f7352fbbd07db9b784b365fe28
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-09-17 17:58:47 -0300

    Adicionando paginas

---
+++ DS 16.09/Paciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_16._09
+{
+    class Paciente
+    {
+        private string nome;
+        private string cpf;
+        public int niveldpreferencial; // Quanto maior = Mais preferencial
+
+        public string Retornarnome()
+        {
+            return nome;
+        }
+        public void Pedirnome()
+        {
+            Console.WriteLine("Digite o nome do paciente:");
+            nome = Console.ReadLine();
+        }
+        public string Retornarcpf()
+        {
+            return cpf;
+        }
+        public void Pedircpf()
+        {
+            Console.WriteLine("Digite o CPF do paciente:");
+            cpf = Console.ReadLine();
+        }
+    }
+}
--- DS 16.09/Program.cs
+++ DS 16.09/Program.cs
@@ -9,9 +9,59 @@ namespace DS_16._09
 {
     class Program
     {
-        static void Main()
+        static Paciente[] pacientes = new Paciente[15]; 
+        static void trocarpagina(int pagina) // Funçâo para que direcionar as paginas existentes (1: Adicionar, 2: Listar, 3: Atender).
         {
-            Console.WriteLine("");
+            Console.Clear();
+            switch (pagina)
+            {
+                case 0:
+                    Main();
+                    break;
+                case 1:
+                    paginaadicionar();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    Console.WriteLine("Erro 404 pagina não encontrada");
+                    Main();
+                    break;
+            }
+        }
+        static void Main() //Pagina inicial - redireciona para outras partes do programa
+        {
+            Console.WriteLine("Bem vindo ao hospital, você deseja:");
+            Console.WriteLine("1. Adicionar um paciente;\n2. Ver todos os pacientes atuais;\n3. Atender um paciente;\n");
+            int escolha = int.Parse(Console.ReadLine());
+            trocarpagina(escolha);
+        }
+        static void paginaadicionar()
+        {
+            Paciente pacientenovo = new Paciente();
+            pacientenovo.Pedirnome();
+            do
+            {
+                Console.WriteLine("Qual é o preferencial do paciente? (Note: Maior = Mais preferencial)");
+                pacientenovo.nivelpreferencial = int.Parse(Console.ReadLine());
+
+                if (pacientenovo.nivelpreferencial < 0)
+                {
+                    Console.WriteLine("Você não colocou um número permitido.");
+                }
+            } while (pacientenovo.nivelpreferencial < 0);
+
+            for (int i = 0; i < pacientes.Length; i++)
+            {
+                if (pacientes[i] == null || pacientes[i].nivelpreferencial <= pacientenovo.nivelpreferencial)
+                {
+
+                }
+            }
         }
     }
 }
